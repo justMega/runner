0173fd91d8ca95e5fdd7979ab16b35442654a9aa
justMega <maks.jagodic@gmail.com>
2023-10-24 18:41:27 +0200

    added fuzzy testing

--- runner.py
+++ runner.py
@@ -4,6 +4,21 @@ import os
 import time
 import multiprocessing
 
+def compileCpp(programName, program):
+    # Command to compile the C++ program 
+    compileArr = ["g++ -std=c++20 -o", programName, program]
+    compileCmd = " ".join(str(x) for x in compileArr)
+    # Compile the C++ program 
+    compileProcess = subprocess.run(compileCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    # Check if the compilation was not successful 
+    if compileProcess.returncode != 0:
+        # Print the compilation error messages 
+        print("Compilation failed.") 
+        print(compileProcess.stderr.decode())
+        exit(1)
+    print("Compilation successful.") 
+    return compileProcess
+
 def getFiles(testFolder, fileExtension):
     out = dict() 
     for file in os.listdir(testFolder):
@@ -15,7 +30,12 @@ def getFiles(testFolder, fileExtension):
             out[numb] = file 
     return out
 
-def runFunc(testName, testNumb, resName, testFolder, program):
+def runFunc(inData, program):
+    runProcess = subprocess.run(f"./{program}", input=bytes(inData, "utf-8") ,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output = runProcess.stdout.decode() 
+    return output
+
+def runFuncFile(testName, testNumb, resName, testFolder, program):
     inF = open(testFolder +"/"+ testName, "rb")
     inFile = inF.read()
     startTime = time.time()
@@ -44,8 +64,13 @@ def runFunc(testName, testNumb, resName, testFolder, program):
 parser = argparse.ArgumentParser()
 parser.add_argument("programName", help="name of the script you want to run ending in .cpp")
 parser.add_argument("testFolder", help="folder where the test are located")
-parser.add_argument("-i", "--interval", help="define interval as x1-x2",)
+
+parser.add_argument("workingProgram",nargs="?", default=".", help="OPTIONAL (use with -f/--fuzzy only!!) program which is correct ending in .cpp")
+parser.add_argument("testGenProgram", nargs="?", default=".", help="OPTIONAL (use with -f/--fuzzy only!!) program that generetes input ending in .py")
+
+parser.add_argument("-f", "--fuzzy", help="enable fuzzy testing", action='store_true')
 parser.add_argument("-t", "--timeoutTime", help="define timeout time", type=float)
+parser.add_argument("-i", "--interval", help="define interval as x1-x2 or if used with -f/--fuzzy testing just x1",)
 args = parser.parse_args()
 
 program = args.programName
@@ -57,44 +82,66 @@ testTime = 2
 if args.timeoutTime:
     testTime = args.timeoutTime
 
-# Command to compile the C++ program 
-compileArr = ["g++ -std=c++20 -o", programName, program]
-compileCmd = " ".join(str(x) for x in compileArr)
-# Compile the C++ program 
-compileProcess = subprocess.run(compileCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+compileProcess = compileCpp(programName, program)
 
-# Check if the compilation was not successful 
-if compileProcess.returncode != 0:
-    # Print the compilation error messages 
-    print("Compilation failed.") 
-    print(compileProcess.stderr.decode())
-    exit(1)
+if args.fuzzy:
+    workingProgram = args.workingProgram
+    testGenProgram = args.testGenProgram
+    if workingProgram == "." or testGenProgram == ".":
+        print("Please list all argument required for fuzzing")
+        exit(1)
+    workingProgramName = (workingProgram.split("/")[-1]).replace(".cpp","")
+    compileProcess = compileCpp(workingProgramName, workingProgram)
 
-print("Compilation successful.") 
-tests = getFiles(testFolder, ".in")
-results = getFiles(testFolder, ".out")
-testInterval = sorted(tests.keys())
-
-try:
+    interval = 20
     if args.interval:
-        interval = args.interval.split("-")
-        testInterval = range(int(interval[0]), int(interval[1])+1)
-except:        
-    print("Parameter not deffined correctly use -h for help")
-    exit(1)
+        print(args.interval)
+        interval = int(args.interval)
 
-for testNumb in testInterval:
-    testName = tests[testNumb]
-    resName = results[testNumb]
-    
-    startTime = time.time()
-    p = multiprocessing.Process(target=runFunc, name="runFunc", args=(testName, testNumb,resName, testFolder, programName))
-    p.start() 
-    while p.is_alive() and time.time() - startTime <= testTime:
-        pass
-    if p.is_alive():
-        outStr = str(testNumb)+"."
-        print(f"\033[1;35m{outStr:<15} [T]\033[0;0m")
-    p.terminate()
-    p.join()
-    
+    compileWorkingProcess = compileCpp(workingProgramName, workingProgram)
+    for i in range(interval):
+        runCmd = f"python3 {testGenProgram}"
+        runPython = subprocess.run(runCmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        test = runPython.stdout.decode()
+
+        results = runFunc(test, workingProgramName)
+        posibleResults = runFunc(test, programName)
+
+        outStr = str(i+1)+"."
+        if results.strip() == posibleResults.strip():
+            print(f"\033[1;32m{outStr:<15} [*]\033[0;0m")
+        else:
+            print(f"\033[1;31m{outStr:<15} [X]\033[0;0m")
+            fileName = f"fuzzyTest{i+1}.in"
+            f = open(fileName, "w")
+            print(test)
+            f.write(test)
+
+else:
+    tests = getFiles(testFolder, ".in")
+    results = getFiles(testFolder, ".out")
+    testInterval = sorted(tests.keys())
+
+    try:
+        if args.interval:
+            interval = args.interval.split("-")
+            testInterval = range(int(interval[0]), int(interval[1])+1)
+    except:        
+        print("Parameter not deffined correctly use -h for help")
+        exit(1)
+
+    for testNumb in testInterval:
+        testName = tests[testNumb]
+        resName = results[testNumb]
+        
+        startTime = time.time()
+        p = multiprocessing.Process(target=runFuncFile, name="runFunc", args=(testName, testNumb,resName, testFolder, programName))
+        p.start() 
+        while p.is_alive() and time.time() - startTime <= testTime:
+            pass
+        if p.is_alive():
+            outStr = str(testNumb)+"."
+            print(f"\033[1;35m{outStr:<15} [T]\033[0;0m")
+        p.terminate()
+        p.join()
+        
